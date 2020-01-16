faa4057b6584efdee3d1abead4ba3510e4051e4e
Michele Lanza <michele_lanza1@hotmail.com>
2020-01-16 14:46:58 -0400

    Subiendo script bd

--- db script.sql
+++ db script.sql
@@ -1,9 +1,8 @@
-select I.inc_name,I.inc_description,I.inc_date, S.sol_date, S.sol_decription ,A.are_name, P.pla_name
-from Incidencia I , Solucion S,Area A, Plataforma P
-where A.are_id = I.fk_area_id AND P.pla_id = I.fk_plataforma_id AND
- S.sol_id = I.fk_solucion_id OR I.fk_solucion_id = null;
+--CREATE DB
 
+CREATE DATABASE bdconocimiento
 
+--create 
 CREATE TABLE Incidencia
 (
     inc_id int identity PRIMARY KEY,
@@ -21,47 +20,30 @@ CREATE TABLE Plataforma
     
 );
 
-
 CREATE TABLE Area
 (
     are_id int identity PRIMARY KEY,
     are_name nvarchar(255) NOT NULL
     
 );
-/*
-CREATE TABLE Solucion
-(
-    sol_id int identity PRIMARY KEY,
-    sol_decription nvarchar(1000) NOT NULL,
-    sol_date datetime, 
 
-);*/
- 
---alter table Incidencia add   inc_solid int not null;
---alter table Incidencia add   inc_soldesc nvarchar(1000) not null;
- 
---alter table Incidencia add   fk_solucion_id int;
+
+--alters
+
 alter table Incidencia add   fk_plataforma_id int not null;
+
 alter table Incidencia add   fk_area_id int not null;
 
 
 alter table Incidencia add Foreign key(fk_area_id)
  REFERENCES Area(are_id);
 
---alter table Incidencia add Foreign key(fk_solucion_id)
- --REFERENCES Solucion(sol_id);
-
-
 alter table Incidencia add Foreign key(fk_plataforma_id)
  REFERENCES Plataforma(pla_id);
-   
- /* alter table Incidencia drop column inc_date;
-  alter table Solucion drop column sol_date;
-  alter table Solucion add sol_date datetime not null;
-  alter table Incidencia add inc_date datetime not null;*/
-  
-  
-  
+ 
+--Inserts
+
+ 
  insert into Area (are_name) values ('Seguridad de la informacion');
  insert into Area (are_name) values ('Servidores'); 
  insert into Area (are_name) values ('Base de Datos');
@@ -75,93 +57,176 @@ alter table Incidencia add Foreign key(fk_plataforma_id)
  insert into Plataforma (pla_name) values ('CDP');
  
  
- 
- 
- ----
- 
--- pla: IBS, AS400, CDP
- 
- 
- 
-insert into Incidencia(inc_name,inc_description,inc_date,fk_solucion_id,fk_plataforma_id,fk_area_id)
- values (?,?,?,?,?,?);
- 
- 
-insert into Solucion(sol_decription,sol_date)
- values (?, ?);
+----SP
 
 
- 
- 
- 
-CREATE OR REPLACE PROCEDURE m03_getcampaignsall ()
+-- {CALL  addIncidencia(?,?,?,?,?,?,?)}
 
-    RETURNS TABLE( inc_name nvarchar, inc_description character varying, inc_date TimeStamp , sol_date TimeStamp,
-                sol_decription nvarchar, are_name nvarchar,pla_name nvarchar )
-				BEGIN 
-
-SELECT  I.inc_name,I.inc_description,I.inc_date, S.sol_date, S.sol_decription ,A.are_name, P.pla_name
-		FROM Incidencia I , Solucion S,Area A, Plataforma P
-		WHERE A.are_id = I.fk_area_id AND P.pla_id = I.fk_plataforma_id AND
-	 	S.sol_id = I.fk_solucion_id OR I.fk_solucion_id = null;
-END;
+CREATE PROCEDURE addIncidencia( @name nVARCHAR(255), @description nVARCHAR(1000), @Idate datetime, 
+							    @soldate datetime, @soldesc  nVARCHAR,	@fk_pl int, 
+							    @fk_are int	)
+AS
+BEGIN
+SET NOCOUNT ON
+ 
+insert into Incidencia(inc_name, inc_description, inc_date, inc_soldate, inc_soldesc,
+			fk_plataforma_id, fk_area_id) values ( @name, @description, @Idate, @soldate, @soldesc, @fk_pl, @fk_are);
+ 
+END
 
 
 
-insert into 
-Incidencia(inc_name,inc_description,inc_date,fk_plataforma_id,fk_area_id,fk_solucion_id)
- values (?,?,?,?,?,?)
+--{CALL getAll()}
+
+CREATE PROCEDURE getAll
+AS
+BEGIN
+SET NOCOUNT ON
+
+SELECT I.inc_id, I.inc_name,I.inc_description,I.inc_date, I.inc_soldate, I.inc_soldesc ,A.are_id, A.are_name,pla_id, P.pla_name
+FROM Incidencia I, Area A, Plataforma P
+WHERE A.are_id = I.fk_area_id AND P.pla_id = I.fk_plataforma_id
+ORDER BY  I.inc_id DESC
+
+END
+
+--{CALL getIncidencia(?)}
+
+CREATE PROCEDURE getIncidencia( @id int )
+AS
+BEGIN
+SET NOCOUNT ON
+
+SELECT I.* , A.*,P.*
+FROM Incidencia I, Area A, Plataforma P 
+WHERE I.inc_id = @id   and A.are_id = I.fk_area_id AND P.pla_id = I.fk_plataforma_id
+ 
+END
+
+
+--{CALL getArea()}
+CREATE PROCEDURE getArea
+AS
+BEGIN
+SET NOCOUNT ON
+ 
+SELECT are_id, are_name
+FROM Area
+ 
+END
+
+-- {CALL getPlat()}
+CREATE PROCEDURE getPlat
+AS
+BEGIN
+SET NOCOUNT ON
+
+SELECT pla_id, pla_name 
+FROM Plataforma
+ 
+END
+
+-- {CALL updateInc( ?,?,?,?,?,?,?,? )}
+
+CREATE PROCEDURE updateInc( @name nVARCHAR (255), @description nVARCHAR(1000), @Idate datetime, 
+							@soldate datetime, @soldesc  nVARCHAR(1000),	@fk_pl int, 
+						    @fk_are int, @id int)
+AS
+BEGIN
+SET NOCOUNT ON
+
+UPDATE Incidencia
+SET  inc_name= @name, 
+     inc_description= @description, 
+	 inc_date= @Idate, 
+     inc_soldate= @soldate, 
+	 inc_soldesc= @soldesc, 
+	 fk_plataforma_id= 	@fk_pl,
+	 fk_area_id = @fk_are
+	 WHERE inc_id = @id 
 
  
- UPDATE Incidencia
-		SET inc_name= ?, 
-		inc_description=?, 
-		inc_date= ?, 
-		fk_plataforma_id= ?, 
-		fk_area_id = ?,
-		fk_solucion_id= ?
-		WHERE inc_id =?;
+END
+
+
+-- {CALL deleteInc (?)	}
+
+
+CREATE PROCEDURE deleteInc(  @id int )
+AS
+BEGIN
+SET NOCOUNT ON
+
+DELETE 
+FROM incidencia
+WHERE inc_id = @id 
+
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- /*
-     editIncidencia (incidencia): Observable<any> {
-    console.log(incidencia)
-    return this.http.put<any>(endpoint + '/Edit/AddSol',incidencia, httpOptions).pipe(
-      tap((incidencia) => console.log(`Incidencia added w/`)),
-    );
-  }*/
- 
- 
- 
- 
- 
-     --poner en un sp
-  /*  String CREATE_INCIDENCIA = "insert into Incidencia(inc_name, inc_description, inc_date, inc_soldate, inc_soldesc, fk_plataforma_id, fk_area_id) values (?,?,?,?,?,?,?);";
-    String SELECT_ALL_INCIDENCIAS = "select I.inc_id, I.inc_name,I.inc_description,I.inc_date, I.inc_soldate, I.inc_soldesc ,A.are_id, A.are_name,pla_id, P.pla_name\n" +
-                                    "FROM Incidencia I, Area A, Plataforma P\n" +
-                                    "WHERE A.are_id = I.fk_area_id AND P.pla_id = I.fk_plataforma_id" ; //poner en un sp
-    String READ_INCIDENCIA = "select I.* , A.*,P.*  from Incidencia I, Area A, Plataforma P  \n" +
-                             "where I.inc_id = ?  and A.are_id = I.fk_area_id AND P.pla_id = I.fk_plataforma_id";
-    String READ_AREA = "select are_id, are_name from Area";
-    String READ_PLAT = "select pla_id, pla_name from Plataforma";
-    String UPDATE_INCIDENCIA = "UPDATE Incidencia\n" +
-                               "SET inc_name= ?, \n" +
-                               "inc_description= ?, \n" +
-                               "inc_date= ?, \n" +
-                               "inc_soldate= ?, \n" +
-                               "inc_soldesc= ?, \n" +
-                               "fk_plataforma_id= ?, \n" +
-                               "fk_area_id = ?\n" +
-                               "WHERE inc_id = ?"; */
-    
- 
- 
+END
+
+
+
+
+
+---------------------Estadisticas----------------------
+
+
+
+/*
+•	Cantidad de incidencias por área. X
+•	Plataformas que han tenido más incidencias. X
+•	Cantidad de incidencias resueltas por área. X 
+•	Cantidad de incidencias no resueltas por área. X
+•	Cantidad de incidencias por mes. 
+
+*/
+
+
+--INCIDENCIAS POR PLATAFORMA
+
+select count(I.inc_id), P.pla_name  
+from Incidencia as I, Plataforma as P 
+where I.fk_plataforma_id = P.pla_id
+Group By  P.pla_name
+Order BY  count(I.inc_id) DESC
+
+--INCIDENCIAS POR AREA
+select count(I.inc_id), A.are_name  
+from Incidencia as I, Area as A 
+where I.fk_area_id = A.are_id
+Group By  A.are_name
+Order BY  count(I.inc_id) DESC  
+
+--INCIDENCIAS NO RESUELTAS POR AREA
+select count(I.inc_id), A.are_name  
+from Incidencia as I, Area as A 
+where I.fk_area_id = A.are_id AND inc_soldesc is null
+Group By  A.are_name
+Order BY  count(I.inc_id) DESC  
+
+--INCIDENCIAS RESUELTAS POR AREA
+select count(I.inc_id) as cuenta, A.are_name  
+from Incidencia as I, Area as A 
+where I.fk_area_id = A.are_id AND inc_soldesc is not null
+Group By  A.are_name  
+Order BY  count(I.inc_id) DESC
+
+
+
+-----INCIDENCIAS POR MES ESPECIFICANDO AÑO
+select  DATENAME(MONTH,I.inc_date) as Mes, count(I.inc_id) as Cuenta
+from Incidencia as I
+where  YEAR(I.inc_date ) = 2020  --variable
+Group By  MONTH(I.inc_date), DATENAME(MONTH,I.inc_date)
+
+
+
+--INCIDENCIAS MENSUALES 
+select count(I.inc_id),P.pla_name
+from Incidencia as I, Plataforma as P
+where  DATEPART(month,inc_date ) = 01/*variable*/ and  DATEPART(year,inc_date ) = 2020/*variable*/ 
+       and  I.fk_plataforma_id = P.pla_id
+Group By  P.pla_name
+
+
 
